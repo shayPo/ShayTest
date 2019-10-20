2feaa3a6fbe4e92e0d520b938b39186558bc76b5
ShayPo <shay.poluck@gmail.com>
2019-10-20 20:19:09 +0300

    add Test file

--- app/src/test/java/polak/shay/applicaster/ExampleUnitTest.kt
+++ app/src/test/java/polak/shay/applicaster/ExampleUnitTest.kt
@@ -1,8 +1,11 @@
 package polak.shay.applicaster
 
+import org.junit.After
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
+import polak.shay.applicaster.view_model.MainViewModel
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +13,17 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        val viewModel = MainViewModel()
+    }
+
+    @After
+    fun after(){
+    }
+
+    @Before
+    fun beforeTest() {
     }
 }
